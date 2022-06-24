bf67adfd9e46d8544c3731f7a75ee271ffbf6954
Jarrod Caplan <bejarrodar@gmail.com>
2022-06-24 14:04:00 -0400

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,31 +1,43 @@
 from tkinter import *
-
-class MyApp:
-    def __init__(self, parent):
-        self.myParent = parent  ### (7) remember my parent, the root
-        self.myContainer1 = Frame(parent)
-        self.myContainer1.pack()
-
-        self.button1 = Button(self.myContainer1)
-        self.button1.configure(text="OK", background= "green")
-        self.button1.pack(side=LEFT)
-        self.button1.bind("<Button-1>", self.button1Click) ### (1)
-
-        self.button2 = Button(self.myContainer1)
-        self.button2.configure(text="Cancel", background="red")
-        self.button2.pack(side=RIGHT)
-        self.button2.bind("<Button-1>", self.button2Click) ### (2)
-
-    def button1Click(self, event):    ### (3)
-        if self.button1["background"] == "green": ### (4)
-            self.button1["background"] = "yellow"
-        else:
-            self.button1["background"] = "green"
-
-    def button2Click(self, event):  ### (5)
-        self.myParent.destroy()     ### (6)
+from tkinter import ttk
+from datetime import datetime
+from xml.etree.ElementTree import tostring
 
 
 root = Tk()
-myapp = MyApp(root)
+root.title("Time Clock")
+def update_time():
+    current_time = (datetime.now().strftime("%H:%M:%S"))
+    ttk.Label(mainframe, text=current_time).grid(column=2, row=1, sticky=W)
+    for child in mainframe.winfo_children(): 
+        child.grid_configure(padx=5, pady=5)
+    root.after(1000, update_time)
+
+def login():
+    return
+
+def time_entry():
+    return
+
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+ttk.Label(mainframe, text="Current Time: ").grid(column=1, row=1, sticky=E)
+ttk.Label(mainframe, text="Employee ID: ").grid(column=1, row=2, sticky=W)
+
+id_number = StringVar()
+ttk.Entry(mainframe, textvariable=id_number).grid(column=2, row=2, sticky=E)
+
+ttk.Button(mainframe,text="Manager Login", command=login).grid(column=3, row=3, sticky=(W, E))
+
+ttk.Button(mainframe,text="Punch Clock", command=time_entry).grid(column=1, row=3, sticky=(W, E))
+update_time()
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+root.after(1000, update_time)
 root.mainloop()
